6c4b69d98fc5d1df83c92b7305ecdbefa9b3c519
thanh khiet <cs.thanhkhiet@gmail.com>
2021-08-30 17:06:42 +0700

    deploy to netlify

--- src/components/Messages/Message/Message.js
+++ src/components/Messages/Message/Message.js
@@ -1,6 +1,8 @@
 import './Message.css'
 import React from 'react'
 
+import ReactEmoji from 'react-emoji'
+
 export default function Message({message: {user, text}, name}) {
     let isSentByCurrentUser = false
     const trimmedName = name.trim().toLowerCase()
@@ -14,14 +16,14 @@ export default function Message({message: {user, text}, name}) {
             <div className="messageContainer justifyEnd">
                 <p className="sentText pr-10">{trimmedName}</p>
                 <div className="messageBox backgroundBlue">
-                    <p className="messageText colorWhite">{text}</p>
+                    <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
                 </div>
             </div>
         )
         : (
             <div className="messageContainer justifyStart">
                 <div className="messageBox backgroundLight">
-                    <p className="messageText colorDark">{text}</p>
+                    <p className="messageText colorDark">{ReactEmoji.emojify(text)}</p>
                 </div>
                 <p className="sentText pl-10">{user}</p>
             </div>
